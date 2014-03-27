122dab9462c5576dc0c499dc2194e759559781e7
Arturo Guzman <arturo@kisurfer.com>
2014-03-27 01:58:55 -0400

    extract player movement binding to function

--- src/game.js
+++ src/game.js
@@ -3,8 +3,8 @@
 
   var grid = require('./grid');
   var arena = require('./arena');
-  var creature = require('./creature');
   var infoPanel = require('./info-panel');
+  var creature = require('./creature');
 
   var game = new Phaser.Game(
     grid.pixelWidth,
@@ -13,34 +13,38 @@
     'game',
     { preload: preload, create: create, update: update, render: render }
   );
+  var infoPanelHeight = parseInt(grid.height / 6, 10);
+  var player;
+  var monster;
+  var cursors;
 
 
   function preload() {
   }
 
-  var player;
-  var monster;
-  var cursors;
-
   function create() {
-    var infoPanelHeight = parseInt(grid.height / 6, 10);
+    // measurements are in tiles not in pixels
     arena.draw(game, { height: grid.height - infoPanelHeight  });
     infoPanel.draw(game, { height: infoPanelHeight });
 
     player = creature(game, { text: '@', x: 1, y: 1 });
     monster = creature(game, { text: 'r', x: 10, y: 10 });
 
-    cursors = game.input.keyboard.createCursorKeys();
-    cursors.right.onDown.add(player.moveRight, player);
-    cursors.left.onDown.add(player.moveLeft, player);
-    cursors.up.onDown.add(player.moveUp, player);
-    cursors.down.onDown.add(player.moveDown, player);
+    bindPlayerMovement(player);
   }
 
   function update() {
   }
 
   function render() {
+  }
+
+  function bindPlayerMovement(player) {
+    cursors = game.input.keyboard.createCursorKeys();
+    cursors.right.onDown.add(player.moveRight, player);
+    cursors.left.onDown.add(player.moveLeft, player);
+    cursors.up.onDown.add(player.moveUp, player);
+    cursors.down.onDown.add(player.moveDown, player);
   }
 
 })();
