bcdae31cc9cf07928541df22236de40322adeb5c
Andrew Hlavats <andrew@artgoboom.com>
2014-03-27 02:00:08 -0400

    adding arena js

---
+++ src/arena.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// var grid = require('./grid');
+var _ = require('lodash');
+
+function arena() {
+  var ar = new Arena();
+
+  return ar;
+}
+
+function Arena() {
+}
+
+_.extend(Arena.prototype, {
+  
+});
+
+module.exports = arena;
+
